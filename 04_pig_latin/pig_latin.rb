586ae684b69c9c70f3ae057daf62f0da12526869
Matthew Isaac Lean <isaaclean@creatibyte.com>
2014-12-26 21:45:53 -0800

    Progress on 04

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,41 @@
+AY = 'ay'
+
+def translate(input)
+	firstPart = ''
+	output = ''
+
+	for i in 0..input.length - 1
+		if isVowel(input[i])
+			if i == 0
+				output = input
+				output += AY
+				break
+			elsif
+				temp = input.split(firstPart)
+				output = temp[0]
+				output += firstPart + AY
+				break
+			end
+		end
+
+		firstPart += input[i]
+	end
+
+	return output
+end
+
+#returns true if inputChar is a vowel
+def isVowel(inputChar)
+	vowels = ['a', 'e', 'i', 'o', 'u']
+
+	for i in 0..vowels.length - 1
+		if inputChar == vowels[i]
+			return true
+		end
+	end
+
+	return false
+end
+
+puts(translate('apple'))
+puts(translate('banana'))
